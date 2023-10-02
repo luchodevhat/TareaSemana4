485dd054f5e561a339f1863f95a76c5c3a560c94
Luis Alejandro <alfaroalejandro597@gmail.com>
2023-10-01 21:14:21 -0600

    tarea terminada

---
+++ src/main/java/fidelitas/tarea/TareaApplication.java
@@ -0,0 +1,15 @@
+package fidelitas.tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class TareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaApplication.class, args);
+	}
+
+}
